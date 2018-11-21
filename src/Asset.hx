d8a059cbf778228aa9d19d71b2a934df1864a1d0
Andreas <ciscoheat@gmail.com>
2018-11-21 22:18:38 +0100

    Adding middleware logger.

--- src/Asset.hx
+++ src/Asset.hx
@@ -1,5 +1,7 @@
 import ds.ImmutableArray;
+import ds.Action;
 import mithril.M;
+using DateTools;
 
 typedef Wheel = {
 	final values : ImmutableArray<String>;
@@ -8,9 +10,16 @@ typedef Wheel = {
 }
 
 class Asset extends DeepState<Wheel> {
-	public function new(values) {
-		super({values: values, currentPosition: 0, spinStart: 0.0});
-		this.subscribeToState((_, _) -> M.redraw());
+    final log = new MiddlewareLog<Wheel>();
+
+	public function new(values) {        
+		super({values: values, currentPosition: 0, spinStart: 0.0}, [log.log]);
+        
+		this.subscribeToState((_, _) -> {
+            M.redraw();
+            var last = log.logs[log.logs.length-1];
+            trace(last.timestamp.format("[%T]") + " " + last.type);
+        });        
 	}
 
 	public function valuesUpdated(values) {
@@ -28,4 +37,19 @@ class Asset extends DeepState<Wheel> {
 	public function wheelStarted(time) {
 		return updateIn(state.spinStart, time).spinStart;
 	}
+}
+
+class MiddlewareLog<T> {
+    public function new() {}
+
+    public final logs = new Array<{state: T, type: String, timestamp: Date}>();
+
+    public function log(state: T, next : Action -> T, action : Action) : T {
+        // Get the next state
+        var newState = next(action);
+
+        // Log it and return it unchanged
+        logs.push({state: newState, type: action.type, timestamp: Date.now()});
+        return newState;
+    }
 }
