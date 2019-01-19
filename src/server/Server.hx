1fba159c537aa6a168ac0bafe55fd9d777d5d582
Andreas <ciscoheat@gmail.com>
2019-01-19 12:29:05 +0100

    Simple server routing.

--- src/server/Server.hx
+++ src/server/Server.hx
@@ -1,16 +1,34 @@
+import php.SuperGlobal;
+import php.Lib;
 import haxe.Json;
 import haxe.Http;
 import php.Web;
 
+using DateTools;
+
 class Server
 {
 	static function main() {
-		var url = 'https://api.meetup.com/ovik-dev/events?sign=true&photo-host=public&status=past,upcoming&key=2b2f2e93f54332315755310656a&desc=true';
-		var data = Http.requestUrl(url);
+		var serverVars = Lib.hashOfAssociativeArray(SuperGlobal._SERVER);
+		var pathInfo : String = serverVars.get("PATH_INFO");
+		var paths = pathInfo == null ? [] : pathInfo.split("/").filter(p -> p.length > 0);
 
-		Web.setHeader("Content-type", "application/json");
-		Web.setHeader("Access-Control-Allow-Origin", "*");
-		
-		Sys.print(data);
+		switch paths {
+			case ['events']:
+				var url = 'https://api.meetup.com/ovik-dev/events?sign=true&photo-host=public&status=past,upcoming&key=2b2f2e93f54332315755310656a&desc=true';
+				var data = Http.requestUrl(url);
+
+				var events : ds.ImmutableArray<Event> = cast Json.parse(data);
+				var dateLimit = Date.now().delta(-1000*60*60*24*90).getTime();
+				var newerEvents = events.filter(e -> e.time > dateLimit);
+
+				Web.setHeader("Content-type", "application/json");
+				Web.setHeader("Access-Control-Allow-Origin", "*");
+
+				Sys.print(Json.stringify(newerEvents));
+				
+			case _:
+				Web.setReturnCode(404);
+		}
 	}
 }
